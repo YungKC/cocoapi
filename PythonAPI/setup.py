4f6238f405b0e88c45e2a909470492ae0684a39f
kai <kaicyung@gmail.com>
2017-12-11 11:39:47 -0800

    resolve setup by changing the compile options

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -5,13 +5,16 @@ import numpy as np
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
+# Kai: in order to compile this under MS VC, I need to remove the -W** compiler options
+#      from: extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+#      to:   extra_compile_args=['-std=c99'],
 
 ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
         include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=['-std=c99'],
     )
 ]
 
